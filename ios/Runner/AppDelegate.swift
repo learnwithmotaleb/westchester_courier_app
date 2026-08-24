954c2d84560d44f3a62a08cb4adcbe3413d97c09
Abdul Motaleb <71353762+learnwithmotaleb@users.noreply.github.com>
2026-08-24 12:05:35 +0600

    ios app update and uploaded

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,39 +1,37 @@
-import Flutter
 import UIKit
+import Flutter
 import GoogleMaps
 import flutter_local_notifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
 
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
 
-        FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+        // Google Maps
+        GMSServices.provideAPIKey(
+            "AIzaSyCY_oVLft-gyfzV_6msJ_H790rH6B-UboI"
+        )
+
+        // Local Notifications background/plugin registration
+        FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { registry in
             GeneratedPluginRegistrant.register(with: registry)
         }
 
-        GMSServices.provideAPIKey("AIzaSyCY_oVLft-gyfzV_6msJ_H790rH6B-UboI")
-
+        // Register Flutter plugins once
         GeneratedPluginRegistrant.register(with: self)
 
+        // Notification delegate
         if #available(iOS 10.0, *) {
-          UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+            UNUserNotificationCenter.current().delegate = self
         }
 
         return super.application(
             application,
             didFinishLaunchingWithOptions: launchOptions
-        )
-    }
-
-    func didInitializeImplicitFlutterEngine(
-        _ engineBridge: FlutterImplicitEngineBridge
-    ) {
-        GeneratedPluginRegistrant.register(
-            with: engineBridge.pluginRegistry
         )
     }
 }
